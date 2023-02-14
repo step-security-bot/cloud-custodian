acff3bcc793242d6e96ed1d0629604cdbdbcabe5
Adam Harwell <flux.adam@gmail.com>
2023-02-14 07:44:59 -0600

    releng - correct typo in setup.py python_requires (#8263)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url="https://github.com/cloud-custodian/cloud-custodian",
     license="Apache-2.0",
     packages=find_packages(),
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3, !=3.4, !=3.5, <4'",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3, !=3.4, !=3.5, <4",
     entry_points={
         'console_scripts': [
             'custodian = c7n.cli:main']},
